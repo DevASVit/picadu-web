058a55aebb7c00bff7dde2e0fa21b4c0a104dc57
DevASVit <devasvit@gmail.com>
2020-11-17 19:04:09 +0200

    added ability to publish posts, firebase connected

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBX4COQjeZ-hRELWftPZW29X1uwYuwSQ3c",
+  authDomain: "pikadu-web-1f5ac.firebaseapp.com",
+  databaseURL: "https://pikadu-web-1f5ac.firebaseio.com",
+  projectId: "pikadu-web-1f5ac",
+  storageBucket: "pikadu-web-1f5ac.appspot.com",
+  messagingSenderId: "433442003371",
+  appId: "1:433442003371:web:d897c892a93c2d0f109aee"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 // Создаем переменную, в которую положим кнопку меню
 let menuToggle = document.querySelector('#menu-toggle');
 // Создаем переменную, в которую положим меню
@@ -12,40 +25,40 @@ const loginForm = document.querySelector('.login-form');
 const emailInput = document.querySelector('.login-email');
 const passwordInput = document.querySelector('.login-password');
 const loginSignup = document.querySelector('.login-signup');
-
 const userElem = document.querySelector('.user');
 const userNameElem = document.querySelector('.user-name');
-
 const exitElem = document.querySelector('.exit');
 const editElem = document.querySelector('.edit');
 const editContainer = document.querySelector('.edit-container');
-
 const editUsername = document.querySelector('.edit-username');
 const editPhotoURL = document.querySelector('.edit-photo');
 const userAvatarElem = document.querySelector('.user-avatar');
-
 const postsWrapper = document.querySelector('.posts');
+const btnNewPost = document.querySelector('.button-new-post');
+const addPostElem = document.querySelector('.add-post');
 
 const listUsers = [
   {
     id: '01',
     email: 'devasvit@gmail.com',
     password: '12345',
-    displayName: 'DevASVit'
+    displayName: 'DevASVit',
+    photo: 'https://vesti.ua/wp-content/uploads/2020/05/samaya-krasivaya-devushka-v-mire-800x530.jpg'
   },
   {
     id: '02',
-    email: 'alona@gmail.com',
+    email: 'maks@gmail.com',
     password: '123456',
-    displayName: 'AlonaVit'
+    displayName: 'Maks',
+    photo: 'https://g4.delphi.lv/images/pix/438x438/A-f21cUJAv0/virietis-ideals-47746713.jpg'
   }
 ];
 
 const setUsers = {
   user: null,
-  logIn(email, password,handler) {
+  logIn(email, password, handler) {
     // метод test проверят совпадение
-    if(!regExpValidEmail.test(email)) return alert('email не валиден')
+    if(!regExpValidEmail.test(email)) return alert('email не валиден');
     const user = this.getUser(email);
 
     if(user && user.password === password) {
@@ -56,11 +69,14 @@ const setUsers = {
     }
   },
   logOut(handler) {
-   this.user = null;
-   handler();
+    this.user = null;
+   
+    if(handler) {
+      handler();
+    }
   },
   signUp(email, password, handler) {
-    if(!regExpValidEmail.test(email)) return alert('email не валиден')
+    if(!regExpValidEmail.test(email)) return alert('email не валиден');
     
     //метод trim убирает пробелы слева и справа
     if(!email.trim() || !password.trim()) {
@@ -72,7 +88,10 @@ const setUsers = {
       const user = {email, password, displayName: email.substring(0, email.indexOf('@'))};
       listUsers.push(user);
       this.authorizedUser(user);
-      handler();
+      
+      if(handler) {
+        handler();
+      }
     } else {
       alert('Пользователь с таким email уже зарегистрирован')
     }
@@ -85,7 +104,10 @@ const setUsers = {
     if(userPhoto) {
       this.user.photo = userPhoto;
     }
-    handler();
+    
+    if(handler) {
+      handler();
+    }
   },
   getUser(email) {
     return listUsers.find(item => item.email === email)
@@ -100,8 +122,8 @@ const setPosts = {
     {
       title: 'Заголовок поста1',
       text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит!',
-      tegs: ['свежее', 'новое', 'горячее', 'мое', 'случайность'],
-      author: 'devasvit@gmail.com',
+      tags: ['свежее', 'новое', 'горячее', 'мое', 'случайность'],
+      author: {displayName: 'DevASVit', photo: 'https://vesti.ua/wp-content/uploads/2020/05/samaya-krasivaya-devushka-v-mire-800x530.jpg'},
       date: '11.11.2020, 20:54:00',
       like: 15,
       comments: 20
@@ -109,13 +131,31 @@ const setPosts = {
     {
       title: 'Заголовок поста2',
       text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt inventore facere amet commodi numquam eveniet a consequuntur consequatur vitae cum atque repudiandae perferendis distinctio quisquam alias, quo neque enim beatae officiis tempore labore assumenda itaque. Possimus officiis fugiat, maxime quae quas vitae a aspernatur dignissimos ex molestiae obcaecati ut minima deserunt similique adipisci eligendi, tenetur amet illo. Voluptate, eaque deserunt?',
-      tegs: ['свежее', 'новое', 'мое', 'случайность'],
-      author: 'alona@gmail.com',
+      tags: ['свежее', 'новое', 'мое', 'случайность'],
+      author: {displayName: 'Maks', photo: 'https://g4.delphi.lv/images/pix/438x438/A-f21cUJAv0/virietis-ideals-47746713.jpg'},
       date: '10.11.2020, 20:54:00',
       like: 45,
       comments: 12
     }
-  ]
+  ],
+  addPost(title, text, tags, handler) {
+    this.allPosts.unshift({
+      title,
+      text,
+      tags: tags.split(',').map(item => item.trim()),
+      author: {
+        displayName: setUsers.user.displayName,
+        photo: setUsers.user.photo
+      },
+      date: new Date().toLocaleString(),
+      like: 0,
+      comments: 0
+    });
+    
+    if(handler) {
+      handler();
+    }
+  }
 };
 
 const toggleAuthDom = () => {
@@ -127,25 +167,33 @@ const toggleAuthDom = () => {
     userElem.style.display ='';
     userNameElem.textContent = user.displayName;
     userAvatarElem.src = user.photo || userAvatarElem.src;
+    btnNewPost.classList.add('visible');
   } else {
     loginElem.style.display ='';
     userElem.style.display ='none';
+    btnNewPost.classList.remove('visible');
+    addPostElem.classList.remove('visible');
+    postsWrapper.classList.add('visible');
   }
 };
+
+const showAddPost = () => {
+  addPostElem.classList.add('visible');
+  postsWrapper.classList.remove('visible');
+}
 
 const showAllPosts = () => {
   let postsHTML = '';
   
   // деструктивное присваивание в аргументе вместо post
-  setPosts.allPosts.forEach(({title, text, date, tegs}) => {
+  setPosts.allPosts.forEach(({title, text, date, tags, like, comments, author}) => {
     postsHTML += `
       <section class="post">
         <div class="post-body">
           <h2 class="post-title">${title}</h2>
           <p class="post-text">${text}</p>
           <div class="tags">
-            ${tegs}
-            <a href="#" class="tag">#свежее</a>
+            ${tags.map(tag => `<a href="#" class="tag">#${tag}</a>`)}
           </div>
         </div>
         <div class="post-footer">
@@ -154,13 +202,13 @@ const showAllPosts = () => {
               <svg width="19" height="20" class="icon icon-like">
                 <use xlink:href="img/icons.svg#like"></use>
               </svg>
-              <span class="likes-counter">26</span>
+              <span class="likes-counter">${like}</span>
             </button>
             <button class="post-button comments">
               <svg width="21" height="21" class="icon icon-comment">
                 <use xlink:href="img/icons.svg#comment"></use>
               </svg>
-              <span class="comments-counter">157</span>
+              <span class="comments-counter">${comments}</span>
             </button>
             <button class="post-button save">
               <svg width="19" height="19" class="icon icon-save">
@@ -175,10 +223,10 @@ const showAllPosts = () => {
           </div>
           <div class="post-author">
             <div class="author-about">
-              <a href="#" class="author-username">arteislamov</a>
+              <a href="#" class="author-username">${author.displayName}</a>
               <span class="post-time">${date}</span>
             </div>
-            <a href="#" class="author-link"><img src="img/avatar.jpeg" alt="avatar" class="author-avatar"></a>
+            <a href="#" class="author-link"><img src=${author.photo || "img/avatar.jpeg"} alt="avatar" class="author-avatar"></a>
           </div>
         </div>
       </section>
@@ -186,6 +234,10 @@ const showAllPosts = () => {
   });
 
   postsWrapper.innerHTML = postsHTML;
+
+  addPostElem.classList.remove('visible');
+  postsWrapper.classList.add('visible');
+
 };
 
 const init = () => {
@@ -235,10 +287,34 @@ const init = () => {
     // вешаем класс на меню, когда кликнули по кнопке меню 
     menu.classList.toggle('visible');
   });
+
+  btnNewPost.addEventListener('click', event => {
+    event.preventDefault();
+    showAddPost();
+  });
   
+  addPostElem.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const { title, text, tags } = addPostElem.elements;
+    
+    if (title.value.length < 6) {
+      alert('Слишком короткий заголовок');
+      return;
+    }
+
+    if (text.value.length < 50) {
+      alert('Слишком короткий пост');
+      return;
+    }
+    
+    setPosts.addPost(title.value, text.value, tags.value, showAllPosts);
+    addPostElem.classList.remove('visible');
+    addPostElem.reset();
+  });
+
   showAllPosts();
   toggleAuthDom();
 };
 
 document.addEventListener('DOMContentLoaded', init);
-
